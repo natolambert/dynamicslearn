3c8fba84833b9843d679c30728d46ca0ff995f5e
natolambert <nol5@cornell.edu>
2018-04-02 11:23:05 -0700

    first commit

---
+++ dynamics_crazyflie.py
@@ -0,0 +1,84 @@
+# Compatibility Python 2/3
+from __future__ import division, print_function, absolute_import
+from future.builtins import range
+from builtins import range, super
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Start importing packages
+import numpy as np
+import math
+from system_utils.systems.deterministic.dynamics import Dynamics as Dynamics
+
+__author__ = 'Somil Bansal'
+__version__ = '0.1'
+
+class CrazyFlie(Dynamics):
+    def __init__(self, dt, m=.035, L=.065, Ixx = 2.3951e-5, Iyy = 2.3951e-5, Izz = 3.2347e-5):
+        super().__init__(dt, x_dim=12, u_dim=4)
+
+        # Setup the state indices
+        self.idx_xyz = [0, 1, 2]
+        self.idx_xyz_dot = [3, 4, 5]
+        self.idx_ptp = [6, 7, 8]
+        self.idx_ptp_dot = [9, 10, 11]
+
+        # Setup the parameters
+        self.m = m
+        self.L = L
+        self.Ixx = Ixx
+        self.Iyy = Iyy
+        self.Izz = Izz
+        self.g = 9.81
+
+    def pqr2rpy(self, x0, pqr):
+        rotn_matrix = np.array([[1., math.sin(x0[0]) * math.tan(x0[1]), math.cos(x0[0]) * math.tan(x0[1])],
+                                [0., math.cos(x0[0]), -math.sin(x0[0])],
+                                [0., math.sin(x0[0]) / math.cos(x0[1]), math.cos(x0[0]) / math.cos(x0[1])]])
+        return rotn_matrix.dot(pqr)
+
+    def simulate(self, x, u, t=None):
+        self._enforce_dimension(x, u)
+        dt = self._dt
+        u0 = u
+        x0 = x
+        idx_xyz = self.idx_xyz
+        idx_xyz_dot = self.idx_xyz_dot
+        idx_ptp = self.idx_ptp
+        idx_ptp_dot = self.idx_ptp_dot
+
+        m = self.m
+        L = self.L
+        Ixx = self.Ixx
+        Iyy = self.Iyy
+        Izz = self.Izz
+        g = self.g
+
+        Tx = np.array([Iyy / Ixx - Izz / Ixx, L / Ixx])
+        Ty = np.array([Izz / Iyy - Ixx / Iyy, L / Iyy])
+        Tz = np.array([Ixx / Izz - Iyy / Izz, 1. / Izz])
+
+        # Array containing the forces
+        Fxyz = np.zeros(3)
+        Fxyz[0] = -1 * (math.cos(x0[idx_ptp[0]]) * math.sin(x0[idx_ptp[1]]) * math.cos(x0[idx_ptp[2]]) + math.sin(
+            x0[idx_ptp[0]]) * math.sin(x0[idx_ptp[2]])) * u0[0] / m
+        Fxyz[1] = -1 * (math.cos(x0[idx_ptp[0]]) * math.sin(x0[idx_ptp[1]]) * math.sin(x0[idx_ptp[2]]) - math.sin(
+            x0[idx_ptp[0]]) * math.cos(x0[idx_ptp[2]])) * u0[0] / m
+        Fxyz[2] = g - 1 * (math.cos(x0[idx_ptp[0]]) * math.cos(x0[idx_ptp[1]])) * u0[0] / m
+
+        # Compute the torques
+        t0 = np.array([x0[idx_ptp_dot[1]] * x0[idx_ptp_dot[2]], u0[1]])
+        t1 = np.array([x0[idx_ptp_dot[0]] * x0[idx_ptp_dot[2]], u0[2]])
+        t2 = np.array([x0[idx_ptp_dot[0]] * x0[idx_ptp_dot[1]], u0[3]])
+        Txyz = np.array([Tx.dot(t0), Ty.dot(t1), Tz.dot(t2)])
+
+        x1 = np.zeros(12)
+        x1[idx_xyz_dot] = x0[idx_xyz_dot] + dt * Fxyz
+        x1[idx_ptp_dot] = x0[idx_ptp_dot] + dt * Txyz
+        x1[idx_xyz] = x0[idx_xyz] + dt * x0[idx_xyz_dot]
+        x1[idx_ptp] = x0[idx_ptp] + dt * self.pqr2rpy(x0[idx_ptp], x0[idx_ptp_dot])
+
+        return x1
+
+    @property
+    def non_linear(self):
+        return True
