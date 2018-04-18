7a3fd120bbb3d5d0323eb85219ef41eb2a7d21ba
natolambert <nol5@cornell.edu>
2018-04-17 19:45:19 -0700

    some data utils, least squares learning model (untested)

--- models.py
+++ models.py
@@ -5,30 +5,67 @@
 # Import External Packages
 import numpy as np
 import math
-import torch
 from sklearn.model_selection import train_test_split
 from sklearn import linear_modle
 
-class LearningModel:
-    # class for dynamics learning model
-    # TODO
+# torch packages
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class LeastSquares(LearningModel):
+
+
+class LeastSquares:
     # fits gathered data to the form
     # x_(t+1) = Ax + Bu
+
     def __init__(self):
         self.reg = linear_model.LinearRegression()
 
-    def train(self, next_states, states_actions_prev):
-        self.reg.fit()
+    def train(self, change_states, states_actions_prev):
+        # need to make sure data here is normalized AND the states are all
+        # formed as change of state, rather than arbitary values (this makes sure
+        # that the features are not fit to large vlaues)
+
+        # Xw = y
+        # this works if we stack a array
+        #       z = [x, u]
+        # then can make a matrix
+        #       w ~ [A, B]
+        Z = np.hstack(states_actions_prev[:,0],states_actions_prev[:,1])
+        y = change_states
+
+        self.reg.fit(Z,y)
+        return self.reg.coef_
 
     def predict(self, states_actions_prev):
+        # predicts next state of a state, action pairing
+        # works if it is a list element, or a pre-concatenated array
 
-class NeuralNet(LearningModel):
-    # TODO
+        if (len(states_actions_prev) == 2):
+            vect = np.hstack(states_actions_prev[0],states_actions_prev[1])
+            pred = self.reg.predict(vect)
+        else:
+            pred = self.reg.predict(states_actions_prev)
+        return pred
 
-class GaussianProcess(LearningModel):
+class NeuralNet:
+    # NOTE
+
+    def __init__(self, D_in, D_out, layers, dims):
+        # layers is a list of layer types to initialize the NN to
+        if (len(layers) != len(dims)):
+            raise ValueError('Passed a not matching neural net layers with corresponding dimensions')
+
+        n_lay = len(layers)
+        for (i,l) in enumerate(layers):
+            if (i==0):
+                self.lay
+
+
+class GaussianProcess:
     # TODO
 
 def ComputeAccuracy(truth,prediction):
--- utils_data.py
+++ utils_data.py
@@ -1,7 +1,42 @@
 # file for data utilities
 import numpy as np
+from sklearn.preprocessing import StandardScalar
 
 def stack_pairs(states, actions):
     # returns a list of 2-tuples of state vectors with action vectors
+    # works with change states as well
+    dim_states = np.shape(states)
+    dim_actions = np.shape(actions)
+    if (dim_states[0] != dim_action[0]):
+        raise ValueError('states and actions not same length')
 
-    
+    lst = []
+    for (a,s) in zip(states,actions):
+        lst.append([a,s])
+
+    return lst
+
+def states_to_delta(states):
+    # takes in a array of states, and reurns a array of states that is the     #   difference between the current state and next state.
+    # NOTE: Does not return a value for the first point given
+    # Above is bevause we are trying to model x_t+1 = f(x_t,a_t)
+
+    dim = np.shape(states)
+    delta = np.zeros((dim[0]-1,dim[1]))
+
+    for (i,s) in enumerate(states[1:,:]):
+        delta[i-1,:] = states[i,:]-states[i-1,:]
+
+    return delta
+
+def normalize_states(delta_states, ScaleType = StandardScaler, x_dim = 12):
+    # normalizes states to standard scalars
+    # delta states should be a n by x_dim array
+    # NOTE: I chose to implement this function because there may be a time when
+    #   we want to scale different states differently, and this is the place to do it
+
+    if (x_dim != 12):
+        raise ValueError('normalize_states not designed for this state vector')
+    scaler = ScaleType()
+    scaled = scaler.fit(delta_states)
+    return scaled
