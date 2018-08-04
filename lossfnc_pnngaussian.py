added swish activation, fixed prob loss func, added param sweeping script, added weighting to loss func based of param scaling factors

lossfnc_pnngaussian.py
@@ -2,7 +2,8 @@
 
 import torch
 import numpy as np
-
+import math
+import torch.nn as nn
 class PNNLoss_Gaussian(torch.nn.Module):
     '''
     Here is a brief aside on why we want and will use this loss. Essentially, we will incorporate this loss function to include a probablistic nature to the dynamics learning nueral nets. The output of the Probablistic Nueral Net (PNN) or Bayesian Neural Net (BNN) will be both a mean for each trained variable and an associated variance. This loss function will take the mean (u), variance (sig), AND the true trained value (s) to compare against the mean. Stacked variances form Cov matrix
@@ -18,8 +19,18 @@ class PNNLoss_Gaussian(torch.nn.Module):
 
          with a small regularization penalty on term on max_logvar so that it does not grow beyond the training distribution’s maximum output variance, and on the negative of min_logvar so that it does not drop below the training distribution’s minimum output variance.
     '''
+
     def __init__(self):
         super(PNNLoss_Gaussian,self).__init__()
+        
+        
+        self.max_logvar = torch.tensor([0.4654, 1.7603, 0.635, 1.0709, 1.7087])
+        self.min_logvar = torch.tensor([-1.1057, -0.0531, -0.7361, -0.8476, -0.0607])
+        self.scalers    = torch.tensor([4.2254, 4.2254, 1.0, 0.1214, 0.1509])
+        
+        self.initialized_maxmin_logvar = True
+    def print_mmlogvars(self): 
+        print("Max log var: ", self.max_logvar, " Min log var: ", self.min_logvar)
 
     def forward(self, output, target):
         '''
@@ -28,21 +39,43 @@ class PNNLoss_Gaussian(torch.nn.Module):
         var is a vector of variances for each of the respective means
         target is a vector of the target values for each of the mean
         '''
-        l = 0.1
+        l = 10
+
+        
 
         d2 = output.size()[1]
         d = torch.tensor(d2/2, dtype=torch.int32)
 
         mean = output[:,:d]
         logvar = output[:,d:]
+        
+        softplus = nn.Softplus()
+        tmp_logvar = softplus(self.max_logvar - logvar)
+        logvar = self.max_logvar - tmp_logvar
+        tmp_logvar = softplus(logvar - self.min_logvar)
+        logvar = self.min_logvar + tmp_logvar
+
+        #if not self.initialized_maxmin_logvar:
+        #  self.max_logvar = torch.zeros(logvar.shape)
+        #  self.min_logvar = torch.zeros(logvar.shape)
+        #  self.initialized_maxmin_logvar = True
+
+        #for i,var_row in enumerate(logvar):
+        #  for j,var in enumerate(var_row):
+        #    if var > self.max_logvar[i][j]:
+        #      self.max_logvar[i][j] = var
+        #    elif var < self.min_logvar[i][j]:
+        #      self.min_logvar[i][j] = var
+
         var = torch.exp(logvar)
         b_s = mean.size()[0]    # batch size
 
         eps = 1e-9              # Add to variance to avoid 1/0
 
         A = mean - target.expand_as(mean)
+        A.mul_(self.scalers)
         B = torch.div(mean - target.expand_as(mean), var.add(eps))
-
+        B.mul_(self.scalers)
         loss = sum(torch.bmm(A.view(b_s, 1, -1), B.view(b_s, -1, 1)).reshape(-1,1)+l*torch.log(torch.abs(torch.prod(var.add(eps),1)).reshape(-1,1)))
         return loss
 

model_general_nn.py
@@ -16,6 +16,7 @@ from torch.nn import MSELoss
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+from Swish import Swish
 
 
 from lossfnc_pnngaussian import PNNLoss_Gaussian
@@ -30,7 +31,10 @@ class GeneralNN(nn.Module):
         hidden_w = 300,
         input_mode = 'Trajectories',
         pred_mode = 'Next State',
-        ang_trans_idx = []):
+        ang_trans_idx = [],
+        depth = 2,
+        activation = "ReLU",
+        B = 1.0):
 
         super(GeneralNN, self).__init__()
         """
@@ -57,7 +61,9 @@ class GeneralNN(nn.Module):
         self.pred_mode = pred_mode
         self.ang_trans_idx = ang_trans_idx
         self.state_idx_l = state_idx_l
-
+        self.depth = depth
+        self.activation = activation
+        self.B = B
         # increases number of inputs and outputs if cos/sin is used
         # plus 1 per angle because they need a pair (cos, sin) for each output
         if len(self.ang_trans_idx) > 0:
@@ -84,15 +90,83 @@ class GeneralNN(nn.Module):
 
         # Sequential object of network
         # The last layer has double the output's to include a variance on the estimate for every variable
-        self.features = nn.Sequential(
-            nn.Linear(self.n_in, hidden_w),
-            nn.ReLU(),
-            nn.Linear(hidden_w, hidden_w),
-            nn.ReLU(),
-            nn.Linear(hidden_w, hidden_w),
-            nn.ReLU(),
-            nn.Linear(hidden_w, self.n_out)
-        )
+        
+        if self.activation == "ReLU":
+            if self.depth == 1:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+            elif self.depth == 2:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+            elif self.depth == 3:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+            elif self.depth == 4:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, hidden_w),
+                    nn.ReLU(),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+        elif self.activation == "Swish":
+            if self.depth == 1:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+            elif self.depth == 2:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+            elif self.depth == 3:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, self.n_out)
+                )
+            elif self.depth == 4:
+                self.features = nn.Sequential(
+                    nn.Linear(self.n_in, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, hidden_w),
+                    Swish(self.B),
+                    nn.Linear(hidden_w, self.n_out)
+                )
 
     def forward(self, x):
         """
@@ -203,16 +277,16 @@ class GeneralNN(nn.Module):
         self.scalardX.fit(dX)
 
         import matplotlib.pyplot as plt
-        plt.hist(dX[:,0], bins=100)
-        plt.hist(dX[:,1], bins=100)
-        plt.hist(dX[:,2], bins=100)
-        plt.hist(dX[:,3], bins=100)
-        plt.hist(dX[:,4], bins=100)
+        #plt.hist(dX[:,0], bins=100)
+        #plt.hist(dX[:,1], bins=100)
+        #plt.hist(dX[:,2], bins=100)
+        #plt.hist(dX[:,3], bins=100)
+        #plt.hist(dX[:,4], bins=100)
         #plt.show()
-        plt.hist(U[:,0], bins=100)
-        plt.hist(U[:,1], bins=100)
-        plt.hist(U[:,2], bins=100)
-        plt.hist(U[:,3], bins=100)
+        #plt.hist(U[:,0], bins=100)
+        #plt.hist(U[:,1], bins=100)
+        #plt.hist(U[:,2], bins=100)
+        #plt.hist(U[:,3], bins=100)
         #plt.show()
         #Normalizing to zero mean and unit variance
         normX = self.scalarX.transform(X)
@@ -225,17 +299,17 @@ class GeneralNN(nn.Module):
         #for i in normU: print(i)
 
 
-        plt.hist(normdX[:,0], bins=100)
-        plt.hist(normdX[:,1], bins=100)
-        plt.hist(normdX[:,2], bins=100)
-        plt.hist(normdX[:,3], bins=100)
-        plt.hist(normdX[:,4], bins=100)
+        #plt.hist(normdX[:,0], bins=100)
+        #plt.hist(normdX[:,1], bins=100)
+        #plt.hist(normdX[:,2], bins=100)
+        #plt.hist(normdX[:,3], bins=100)
+        #plt.hist(normdX[:,4], bins=100)
         #plt.hist(normdX[:,5], bins=100)
         #plt.show()
-        plt.hist(normU[:,0], bins=100)
-        plt.hist(normU[:,1], bins=100)
-        plt.hist(normU[:,2], bins=100)
-        plt.hist(normU[:,3], bins=100)
+        #plt.hist(normU[:,0], bins=100)
+        #plt.hist(normU[:,1], bins=100)
+        #plt.hist(normU[:,2], bins=100)
+        #plt.hist(normU[:,3], bins=100)
         #plt.show()
         #quit()
         inputs = torch.Tensor(np.concatenate((normX, normU), axis=1))
@@ -344,7 +418,7 @@ class GeneralNN(nn.Module):
             NNout = self.postprocess(NNout[:int(self.n_out/2)]).ravel()
         else:
             NNout = self.postprocess(NNout).ravel()
-
+        
         return NNout
 
     def _optimize(self, loss_fn, optim, epochs, batch_size, trainLoader, testLoader):
@@ -387,6 +461,7 @@ class GeneralNN(nn.Module):
             #          "test_error={:.9f}".format(test_error))
             print("Epoch:", '%04d' % (epoch + 1), "train loss=", "{:.6f}".format(avg_loss.data[0]), "test loss=", "{:.6f}".format(test_error))
             errors.append(test_error)
+        #loss_fn.print_mmlogvars()
         return errors
 
     def save_model(self, filepath):

script_testing.py
@@ -1,14 +1,12 @@
-width = 100
-epochs = 100
+width = 250
+epochs = 25
 batch  = 32
-learning_rate = 8e-7
+learning_rate = 5e-6
 prob = True
-data_name  = '250hz-clean'
+data_name  = 'pink_long_clean'
 
 using_premade_data = False
 old_model = False
-
-
 
 from dynamics import *
 import pickle
@@ -107,12 +105,15 @@ elif runType is RunType.CF:
   imu = unpack_cf_imu(states[:,1], states[:,0]) # linear and angular acceleration
 
   Seqs_X = np.concatenate([imu[:,3:], states[:,2:4]], 1)
+  #Seqs_X = np.concatenate([states[:,2:3]], 1)
   #Seqs_X = np.concatenate([imu, states[:,2:]], 1)
   #Seqs_X = np.concatenate([imu, states], 1) # linear accel, angular accel, and YPR
   #Seqs_X = states # YPR ONLY
   Seqs_U = unpack_cf_pwm(data[:,3])
   #print(np.shape(Seqs_U))
   print(np.mean(Seqs_U,axis=0))
+
+
 if len(Seqs_X.shape) < 3:
   print("added padding dimension to Seqs_X")
   Seqs_X = np.expand_dims(Seqs_X, axis=0)
@@ -122,8 +123,6 @@ if len(Seqs_U.shape) < 3:
 
 
 print("Data : \n", Seqs_X, Seqs_U)
-#
-#
 np.savez('_simmed_data/testingfile_generalnn.npz', Seqs_X, Seqs_U)
 
 print('.... loading training data')
@@ -151,7 +150,8 @@ forces_learn = ['Thrust', 'taux', 'tauy']
 
 
 if runType is RunType.CF:
-  newNN = GeneralNN(n_in_input = 4, n_in_state = 5, hidden_w=width, n_out = 5, state_idx_l=[0,1,2,3,4], prob=prob, pred_mode = 'Next State')#, ang_trans_idx =[0,1,2])
+  newNN = GeneralNN(n_in_input = 4, n_in_state = 5, hidden_w=width, n_out = 5, state_idx_l=[0,1,2,3,4], prob=prob, pred_mode = 'Delta State', depth=2, activation="Swish", B = 1.0)#, ang_trans_idx =[0,1,2])
+  print(newNN)
 elif runType is RunType.IONO:
   #newNN = GeneralNN(n_in_input = 4, n_in_state = 6, n_out = 6, state_idx_l=[0,1,2,3,4,5], prob=False, pred_mode = 'Next State')#, ang_trans_idx =[0,1,2])
   newNN = GeneralNN(n_in_input = 4, n_in_state = 6, n_out = 6, state_idx_l=[0,1,2,3,4,5], prob=True, pred_mode = 'Next State')#, ang_trans_idx =[0,1,2])
@@ -213,7 +213,7 @@ time.sleep(2)
 
 #print('Loading as new model')
 if old_model:
-  newNN = torch.load('_models/model.pth')
+  newNN = torch.load('_models/w-150e-95lr-7e-06b-64d-pinkp-True.pth')
 #if runType is RunType.CF:
   #acc2 = newNN2.train((Seqs_X, Seqs_U), learning_rate=2.5e-5, epochs=25, batch_size = 100, optim="Adam")
 #else:
@@ -279,20 +279,34 @@ plt.show()
 
 
 
-#plot_model(data, newNN, 0, model_dims = ypr, delta=True)
-#plot_model(data, newNN, 1, model_dims = ypr, delta=True)
-#plot_model(data, newNN, 2, model_dims = ypr, delta=True)
-#plot_model(data, newNN, 3, model_dims = ypr, delta=True)
-#plot_model(data, newNN, 4, model_dims = ypr, delta=True)
-
-#plot_model(data, newNN, 0, model_dims = ypr, delta=False)
-#plot_model(data, newNN, 1, model_dims = ypr, delta=False)
-#plot_model(data, newNN, 2, model_dims = ypr, delta=False)
-plot_model(data, newNN, 3, model_dims = ypr, delta=False)
-plot_model(data, newNN, 4, model_dims = ypr, delta=False)
+plot_model(data[int(0.8*len(data)):], newNN, 0, model_dims = ypr, delta=True, sort = False)
+plot_model(data[int(0.8*len(data)):], newNN, 1, model_dims = ypr, delta=True, sort = False)
+plot_model(data[int(0.8*len(data)):], newNN, 2, model_dims = ypr, delta=True, sort = False)
+plot_model(data[int(0.8*len(data)):], newNN, 3, model_dims = ypr, delta=True, sort = False)
+plot_model(data[int(0.8*len(data)):], newNN, 4, model_dims = ypr, delta=True, sort = False)
 plt.show()
+plot_model(data[int(0.8*len(data)):], newNN, 0, model_dims = ypr, delta=True, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 1, model_dims = ypr, delta=True, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 2, model_dims = ypr, delta=True, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 3, model_dims = ypr, delta=True, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 4, model_dims = ypr, delta=True, sort = True)
+plt.show()
+
+
+#plot_model(data[int(0.8*len(data)):], newNN, 0, model_dims = ypr, delta=False, sort = False)
+#plot_model(data[int(0.8*len(data)):], newNN, 1, model_dims = ypr, delta=False, sort = False)
+#plot_model(data[int(0.8*len(data)):], newNN, 2, model_dims = ypr, delta=False, sort = False)
+#plot_model(data[int(0.8*len(data)):], newNN, 3, model_dims = ypr, delta=False, sort = False)
+plot_model(data[int(0.8*len(data)):], newNN, 4, model_dims = ypr, delta=False, sort = False)
+plt.show()
+plot_model(data[int(0.8*len(data)):], newNN, 0, model_dims = ypr, delta=False, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 1, model_dims = ypr, delta=False, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 2, model_dims = ypr, delta=False, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 3, model_dims = ypr, delta=False, sort = True)
+plot_model(data[int(0.8*len(data)):], newNN, 4, model_dims = ypr, delta=False, sort = True)
 #plot_trajectories_state(Seqs_X, 2)
 
+plt.show()
 quit()
 
 ################################ Obj Fnc ################################
@@ -314,7 +328,7 @@ x_controlled, u_seq = sim_sequence(crazy1, dt_m, dt_u, controller = mpc1, sequen
 
 # x0 = np.zeros(12)
 # new_seq, Us = sim_sequence(iono1, dt_u, sequence_len = 150, x0=x0, controller = mpc1)
-#
+##
 # compareTraj(Us, x0, iono1, nn, show=True)
 ################################ Sim Controlled ################################
 

utils_data.py
@@ -38,7 +38,7 @@ def stack_trios(change_states, states, actions):
     lst = []
     for (d,a,s) in zip(change_states,states,actions):
         lst.append([d,a,s])
-
+    print('das shape: ', np.array(lst).shape)
     return np.array(lst)
 
 def states2delta(states):
@@ -85,9 +85,9 @@ def sequencesXU2array(Seqs_X, Seqs_U, normalize = False):
     # data = np.zeros(((n-1)*l,dimx+dimu))
     seqs = []
     Seqs_dX = Seqs_X[:,1:,:]-Seqs_X[:,:-1,:]
-    # print(np.shape(Seqs_dX))
-    # print(np.shape(Seqs_X))
-    # print(np.shape(Seqs_U))
+    print(np.shape(Seqs_dX))
+    print(np.shape(Seqs_X))
+    print(np.shape(Seqs_U))
 
     for (seqdX, seqX, seqU) in zip(Seqs_dX, Seqs_X,Seqs_U):
         # generates the changes in states from raw data
@@ -100,8 +100,13 @@ def sequencesXU2array(Seqs_X, Seqs_U, normalize = False):
         dx_x_u_t = stack_trios(seqdX[:,:], seqX[:-1,:], seqU[:-1,:])
         seqs.append(dx_x_u_t)
 
+    print(np.shape(seqs))
     # reshape data into a long list of dx, x, u pairs for training
-    data = np.reshape(seqs, (n*(l-1),3))
+
+    if len(np.shape(seqs))  == 4:
+      data = np.reshape(seqs, (n*(l-1),3, dimx))
+    else:
+      data = np.reshape(seqs, (n*(l-1),3))
     return data
 
 def l2array(list_arrays):