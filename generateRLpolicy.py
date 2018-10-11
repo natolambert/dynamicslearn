1b9bb5d0744f177f165c1a968f24762ec68d002a
Nathan Lambert <nol@berkeley.edu>
2018-10-11 08:11:46 -0700

    Prep RL file

--- generateRLpolicy.py
+++ generateRLpolicy.py
@@ -31,27 +31,34 @@ This file will be the hub location for learning a control policy on top of a
    learned dynamcis model. As implementation proceeds, we will take in more
    arguments from the command line that will attempt to learn control policies
    a = pi(s). This type of policy has potential to be substantially less
-   computationally intensive than MPC. 
+   computationally intensive than MPC.
 '''
 
 ######################################################################
+def main():
+    # adding arguments to make code easier to work with
+    parser = argparse.ArgumentParser(description='Run function to train or run a RL policy on a dynamics model')
+    parser.add_argument('policy', type=str,
+                        choices = ['Value', 'Policy'],
+                        help='choose which type of policy search to use.')
+    parser.add_argument('--log', action='store_true',
+                        help='a flag for storing a training log in a txt file')
+    parser.add_argument('--noprint', action='store_false',
+                        help='turn off printing in the terminal window for epochs')
+    parser.add_argument('--plot', action='store_true',
+                        help='plots information for easy analysis')
 
-# adding arguments to make code easier to work with
-parser = argparse.ArgumentParser(description='Engineer PID tuning off learned dynamics model.')
-parser.add_argument('dimension', type=str,
-                    choices = ['pitch', 'roll'],
-                    help='choose which dimension to tune PID for.')
-parser.add_argument('--log', action='store_true',
-                    help='a flag for storing a training log in a txt file')
-parser.add_argument('--noprint', action='store_false',
-                    help='turn off printing in the terminal window for epochs')
-parser.add_argument('--plot', action='store_true',
-                    help='plots information for easy analysis')
+    args = parser.parse_args()
 
-args = parser.parse_args()
-
-log = args.log
-noprint = args.noprint
-ensemble = args.ensemble
+    log = args.log
+    noprint = args.noprint
+    ensemble = args.ensemble
 
 ######################################################################
+
+
+if __name__ == "__main__":
+    print('\n---------------------------------------------------')
+    print('Running file generateRLpolicy')
+    print('\n')
+    main()
