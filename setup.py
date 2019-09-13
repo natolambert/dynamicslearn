13004276abb535a9ae7158a12bd91aa000550670
natolambert <nol@berkeley.edu>
2019-09-13 14:40:51 -0700

    basic setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-# TODO
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='dynamicslearn',
+    version='0.1dev',
+    packages=find_packages(),
+    license='MIT License',
+    long_description=open('README.md').read(),
+)  
